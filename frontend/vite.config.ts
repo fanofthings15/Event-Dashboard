dca1440f5b99e17a0eb6af556895be0c01744327
fanofthings15 <newo1515@gmail.com>
2026-08-13 00:36:36 -0400

    Move frontend dev server off port 5173, make it configurable

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: process.env.FRONTEND_PORT ? Number(process.env.FRONTEND_PORT) : 5290,
     proxy: {
       "/api": "http://localhost:3020",
     },
